07aa58fa108f0422ec48265887b024b7d7199ab3
Saul Fernandez <fsaul16@yahoo.com>
2016-03-04 20:38:00 +0000

    test commit

---
+++ app.rb
@@ -0,0 +1,26 @@
+require_relative 'config/environment'
+
+class App < Sinatra::Base
+  
+  get '/' do
+    erb :index
+  end
+  
+  post'/' do
+    @user = {
+      name: "#{params["fname"]} #{params["lname"]}",
+      profile_pic_url: "#{params["picurl"]}",
+      email: "#{params["email"]}",
+      bday: "#{params["bday"]}",
+      quote: "#{params["quote"]}",
+      color: "#{params["color"]}",
+      day: "#{params["day"]}",
+      month: "#{params["month"]}",
+      year: "#{params["year"]}",
+      country: "#{params["country"]}",
+      city: "#{params["city"]}"
+      }
+    erb :dashboard
+  end
+  
+end
